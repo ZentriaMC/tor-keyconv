0e2915b0c1d3fed3f28a47cc7f3887d523b0f34e
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-04-28 22:48:02 +0300

    Do more sanity checks

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
 	"flag"
 	"fmt"
@@ -10,9 +11,19 @@ import (
 	tored25519 "github.com/cretz/bine/torutil/ed25519"
 )
 
+var (
+	hsKeyPrefix = []byte(`== ed25519v1-secret: type0 ==`)
+)
+
 var keyFile string
 
 func main() {
+	if err := entrypoint(); err != nil {
+		panic(err)
+	}
+}
+
+func entrypoint() (err error) {
 	flag.StringVar(&keyFile, "key", "", "Path to Onion service secret key. You're looking for 'hs_ed25519_secret_key'")
 	flag.Parse()
 
@@ -23,9 +34,24 @@ func main() {
 
 	data, err := ioutil.ReadFile(keyFile)
 	if err != nil {
-		panic(fmt.Errorf("failed to read %s: %w", keyFile, err))
+		err = fmt.Errorf("failed to read %s: %w", keyFile, err)
+		return
 	}
 
-	secretKey := tored25519.PrivateKey(data[32:])
-	fmt.Printf("ED25519-V3:%s\n", base64.StdEncoding.EncodeToString(secretKey))
+	// 32 byte header + 64 byte key
+	if l := len(data); l != 96 {
+		err = fmt.Errorf("invalid key - expected len %d, was %d", 96, l)
+		return
+	}
+
+	if !bytes.Equal(data[0:len(hsKeyPrefix)], hsKeyPrefix) {
+		err = fmt.Errorf("invalid key - invalid header")
+		return
+	}
+
+	secretKeyRaw := data[32:] // Start from 32nd
+	secretKey := tored25519.PrivateKey(secretKeyRaw)
+	encoded := base64.StdEncoding.EncodeToString(secretKey)
+	fmt.Printf("ED25519-V3:%s\n", encoded)
+	return
 }
